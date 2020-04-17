ed740e56f5b9af0993e630703cc3d4c297a27c3d
lenovo <lenovo@LAPTOP-P8K9KTT5>
2020-04-17 16:09:46 +0800

    8_2

--- 8_2/8_2.cpp
+++ 8_2/8_2.cpp
@@ -2,10 +2,40 @@
 //
 
 #include "stdafx.h"
-
+#include <opencv2/opencv.hpp>
+using namespace cv;
+using namespace std;
 
 int main()
 {
-    return 0;
+	cv::Mat src = imread("D:\\die_on_chip.png", 0);
+	cv::Mat dst;
+
+	threshold(src, dst, 150, 250, THRESH_BINARY);
+
+	vector<vector<Point>> contours;
+	findContours(dst, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+	for (int i = 0; i < contours.size(); i++) {
+
+		RotatedRect rbox = minAreaRect(contours[i]);
+		rbox.angle = 0;
+		float width = rbox.size.width;
+		float height = rbox.size.height;
+		float a = width / height;
+
+		if (a > 0.9&&a< 1.1) {
+			drawContours(src, contours, i, Scalar(0, 255, 255), 1, 8);
+			cv::Point2f vtx[4];
+			rbox.points(vtx);
+			for (int i = 0; i < 4; ++i) {
+				cv::line(src, vtx[i], vtx[i < 3 ? i + 1 : 0], cv::Scalar(0, 0, 255), 2, CV_AA);
+			}
+		}
+	}
+
+	cv::imshow("src", src);
+	waitKey(0);
+	return 0;
 }
 
